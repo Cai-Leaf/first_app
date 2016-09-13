450d305aee7c517e8763b667494ea7085188247b
Adobe <caicyleaf@163.com>
2016-09-13 13:15:22 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f0eae9bd816fbde3b45c6b9cc742e338e261a3c44bae2bdc50de482fef2d495c5e792589a8480ed193c0fd871e1009c7d7bb0d585156e4edaea517517996d02f'
